547a8359ab352d24549da9557c49ab77231673cb
nkmgauty <gautham@karthik.sg>
2024-09-17 17:23:11 +0530

    erp_stage_002

--- src/Components/Router/Router.jsx
+++ src/Components/Router/Router.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Admin_login from "../Admin_login";
-import Customer_login from "../Customer_login";
+
 import Login from "../Login";
 import Staff_login from "../Staff_login";
 
 function Router() {
   return (
-    
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route exact path="/admin" element={<Admin_login />} />
-          <Route exact path="/customer" element={<Customer_login />} />
-          <Route exact path="/staff" element={<Staff_login />} />
-        </Routes>
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/admin" element={<Admin_login />} />
+
+        <Route exact path="/staff" element={<Staff_login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
